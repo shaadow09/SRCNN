17c9eb130ecb595a209e5b4ebe3da16fd7c18a5d
shaadow09 <1543672738@qq.com>
2019-11-05 21:55:11 +0800

    delete some files

--- low_image/temp.py
+++ low_image/temp.py
@@ -1,8 +1,13 @@
 import cv2 as cv
-import os 
-print(os.listdir())
-img = cv.imread('0.bmp')
-h, w, d = img.shape
-img = cv.resize(img ,(int(w/3), int(h/3)))
-img = cv.resize(img , (w, h))
-cv.imwrite("low_baby_GT.bmp", img)
+import numpy as np 
+
+
+def psnr(image, noise):
+    diff = image - noise
+    mse = np.mean(np.square(diff))
+    mse = 10 * np.log10(255 * 255 / mse)    
+    return mse
+
+image = cv.imread('low_woman_GT.bmp')
+img = cv.resize(cv.imread('woman_GT.bmp'), (image.shape[1], image.shape[0]))
+print(psnr(img, image))
